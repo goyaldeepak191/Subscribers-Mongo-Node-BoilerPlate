1340d94c81cda863dff134d4f4cdd1ef7ed38d25
Sushil Kumar <sushil.18je0861@am.iitism.ac.in>
2020-11-27 17:19:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 const express = require('express')
-const app = express()
-const bodyParser = require("body-parser");
+const app = require('./app.js')
+const mongoose = require('mongoose')
 const port = 3000
-app.use(express.urlencoded());
 
 // Parse JSON bodies (as sent by API clients)
-app.use(express.json());
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
 
+// Connect to DATABASE
+const DATABASE_URL = "mongodb://localhost/subscribers";
+mongoose.connect(DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection
+db.on('error', (err) => console.log(err))
+db.once('open', () => console.log('connected to database'))
 
-app.use(bodyParser.urlencoded({ extended: false }))
-
-app.use(bodyParser.json())
-// your code goes here
-
-
+// Start Server
 app.listen(port, () => console.log(`App listening on port ${port}!`))
-
-module.exports = app;
