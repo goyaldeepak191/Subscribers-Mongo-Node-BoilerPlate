8e7c72bd131df433a449bb3ef05242a27ed38664
Deepak Goyal <goyaldeepak191@gmail.com>
2020-12-23 12:51:29 +0530

    abc

--- src/app.js
+++ src/app.js
@@ -4,6 +4,34 @@ const app = express()
 
 
 // Your code goes here
+const SubscriberModel = require('./models/subscribers');
+
+app.get( '/subscribers', async ( request, response ) => {
+    response.send( await SubscriberModel.find() );
+} );
+
+app.get( '/subscribers/names', async ( request, response ) => {
+    const fullResults = await SubscriberModel.find().select( {
+        _id: false,
+        subscribedDate: false,
+        __v:false
+    } );
+    response.send( fullResults );
+} );
+
+app.get( '/subscribers/:id', async ( request, response ) => {
+    const idTosearch = request.params.id;
+    try {
+        const doc = await SubscriberModel.findOne( { _id: idTosearch } );
+        if( doc == null ) {
+            response.status( 400 ).send( { message: "Id not found" } );
+        } else {
+            response.send( doc );
+        }
+    } catch( error ) {
+        response.status( 400 ).send( { message: error.message } );
+    }
+} );
 
 
 
